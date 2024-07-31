a9fb8cde54c846da91c52b7eec03f64ed04000d1
swarnimvarchaswa <2021031178@mmmut.ac.in>
2024-07-31 09:45:27 +0530

    change in back

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ const server = app.listen(PORT, () => {
 const io = require("socket.io")(server, {
   pingTimeout: 300000,
   cors: {
-    origin: "https://booktrade.onrender.com",
+    origin: "https://booktrade-backend.vercel.app",
     // origin: "http://localhost:3000",
   },
 });
