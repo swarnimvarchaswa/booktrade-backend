043886a79edc50a23413984a2fad7ce02392184f
swarnimvarchaswa <2021031178@mmmut.ac.in>
2024-08-10 17:55:49 +0530

    ommit

--- app.js
+++ app.js
@@ -44,37 +44,37 @@ const server = app.listen(PORT, () => {
   console.log("server is running on " + PORT);
 });
 
-const corsOptions = {
-  origin: ['https://booktrade.onrender.com', // Your frontend domain
-    "https://booktrade-backend.vercel.app" // Backend domain
-  ],
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed methods
-  allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
-  credentials: true, // If you want to allow credentials
-};
+// const corsOptions = {
+//   origin: ['https://booktrade.onrender.com', // Your frontend domain
+//     "https://booktrade-backend.vercel.app" // Backend domain
+//   ],
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed methods
+//   allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+//   credentials: true, // If you want to allow credentials
+// };
 
-app.use(cors(corsOptions));
-
-// const io = require("socket.io")(server, {
-//   pingTimeout: 300000,
-//   cors: {
-//     origin: "https://booktrade-backend.vercel.app",
-//     // origin: "http://localhost:3000",
-//   },
-// });
+// app.use(cors(corsOptions));
 
 const io = require("socket.io")(server, {
   pingTimeout: 300000,
   cors: {
-    origin: [
-      "https://booktrade.onrender.com", // Frontend domain
-      "https://booktrade-backend.vercel.app" // Backend domain
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
+    origin: "https://booktrade-backend.vercel.app",
+    // origin: "http://localhost:3000",
   },
 });
+
+// const io = require("socket.io")(server, {
+//   pingTimeout: 300000,
+//   cors: {
+//     origin: [
+//       "https://booktrade.onrender.com", // Frontend domain
+//       "https://booktrade-backend.vercel.app" // Backend domain
+//     ],
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     allowedHeaders: ["Content-Type", "Authorization"],
+//     credentials: true,
+//   },
+// });
 
 
 const activeChats = new Set();
